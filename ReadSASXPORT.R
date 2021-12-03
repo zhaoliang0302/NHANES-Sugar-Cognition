22b4992c11d4ed42bf96978149823e02281e4e41
Natalia G Goncalves <95378758+NattyGG@users.noreply.github.com>
2021-12-03 14:45:07 -0300

    Create ReadSASXPORT.R

---
+++ ReadSASXPORT.R
@@ -0,0 +1,111 @@
+library(foreign)
+
+#Reads DEMO data from 2011-2012
+DEMO_G <- read.xport("Path/DEMO_G.XPT") 
+#Saves in csv
+write.csv(DEMO_G, file="Path/DEMO_G.csv") 
+
+#Reads DEMO data from 2013-2014
+DEMO_H <- read.xport("Path/DEMO_H.XPT") 
+#Saves in csv
+write.csv(DEMO_H, file="Path/DEMO_H.csv")
+
+#Reads DR1IFF data from 2011-2012
+DR1IFF_G <- read.xport("Path/DR1IFF_G.XPT") 
+#Saves in csv
+write.csv(DR1IFF_G, file="Path/DR1IFF_G.csv") 
+
+#Reads DR1IFF data from 2013-2014
+DR1IFF_H <- read.xport("Path/DR1IFF_H.XPT") 
+#Saves in csv
+write.csv(DR1IFF_H, file="Path/DR1IFF_H.csv")
+
+#Reads DR1TOT data from 2011-2012
+DR1TOT_G <- read.xport("Path/DR1TOT_G.XPT") 
+#Saves in csv
+write.csv(DR1TOT_G, file="Path/DR1TOT_G.csv") 
+
+#Reads DR1TOT data from 2013-2014
+DR1TOT_H <- read.xport("Path/DR1TOT_H.XPT") 
+#Saves in csv
+write.csv(DR1TOT_H, file="Path/DR1TOT_H.csv")
+
+#Reads BMX data from 2011-2012
+BMX_G <- read.xport("Path/BMX_G.XPT") 
+#Saves in csv
+write.csv(BMX_G, file="Path/BMX_G.csv") 
+
+#Reads BMX data from 2013-2014
+BMX_H <- read.xport("Path/BMX_H.XPT") 
+#Saves in csv
+write.csv(BMX_H, file="Path/BMX_H.csv")
+
+#Reads ALQ data from 2011-2012
+ALQ_G <- read.xport("Path/ALQ_G.XPT") 
+#Saves in csv
+write.csv(ALQ_G, file="Path/ALQ_G.csv") 
+
+#Reads ALQ data from 2013-2014
+ALQ_H <- read.xport("Path/ALQ_H.XPT") 
+#Saves in csv
+write.csv(ALQ_H, file="Path/ALQ_H.csv")
+
+#Reads CFQ data from 2011-2012
+CFQ_G <- read.xport("Path/CFQ_G.XPT") 
+#Saves in csv
+write.csv(CFQ_G, file="Path/CFQ_G.csv") 
+
+#Reads CFQ data from 2013-2014
+CFQ_H <- read.xport("Path/CFQ_H.XPT") 
+#Saves in csv
+write.csv(CFQ_H, file="Path/CFQ_H.csv")
+
+#Reads MCQ data from 2011-2012
+MCQ_G <- read.xport("Path/MCQ_G.XPT") 
+#Saves in csv
+write.csv(MCQ_G, file="Path/MCQ_G.csv") 
+
+#Reads MCQ data from 2013-2014
+MCQ_H <- read.xport("Path/MCQ_H.XPT") 
+#Saves in csv
+write.csv(MCQ_H, file="Path/MCQ_H.csv")
+
+#Reads SMQ data from 2011-2012
+SMQ_G <- read.xport("Path/SMQ_G.XPT") 
+#Saves in csv
+write.csv(SMQ_G, file="Path/SMQ_G.csv") 
+
+#Reads SMQ data from 2013-2014
+SMQ_H <- read.xport("Path/SMQ_H.XPT") 
+#Saves in csv
+write.csv(SMQ_H, file="Path/SMQ_H.csv")
+
+#Reads PAQ data from 2011-2012
+PAQ_G <- read.xport("Path/PAQ_G.XPT") 
+#Saves in csv
+write.csv(PAQ_G, file="Path/PAQ_G.csv") 
+
+#Reads PAQ data from 2013-2014
+PAQ_H <- read.xport("Path/PAQ_H.XPT") 
+#Saves in csv
+write.csv(PAQ_H, file="Path/PAQ_H.csv")
+
+#Reads DIQ data from 2011-2012
+DIQ_G <- read.xport("Path/DIQ_G.XPT") 
+#Saves in csv
+write.csv(DIQ_G, file="Path/DIQ_G.csv") 
+
+#Reads DIQ data from 2013-2014
+DIQ_H <- read.xport("Path/DIQ_H.XPT") 
+#Saves in csv
+write.csv(DIQ_H, file="Path/DIQ_H.csv")
+
+#Reads BPQ data from 2011-2012
+BPQ_G <- read.xport("Path/BPQ_G.XPT") 
+#Saves in csv
+write.csv(BPQ_G, file="Path/BPQ_G.csv") 
+
+#Reads BPQ data from 2013-2014
+BPQ_H <- read.xport("Path/BPQ_H.XPT") 
+#Saves in csv
+write.csv(BPQ_H, file="Path/BPQ_H.csv")
