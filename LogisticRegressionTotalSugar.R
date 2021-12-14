a7b4b030ec3265d63c3b76532511cf285365ef57
Natalia G Goncalves <95378758+NattyGG@users.noreply.github.com>
2021-12-14 11:39:40 -0300

    Update LogisticRegressionTotalSugar.R

--- LogisticRegressionTotalSugar.R
+++ LogisticRegressionTotalSugar.R
@@ -6,18 +6,21 @@ library(survey)
 #Unadjusted
 RLCeradBinarioSugarTercil <- svyglm(CeradBinario ~ as.factor(AcucarTercil), family = quasibinomial, design = survey19)
 summary(RLCeradBinarioSugarTercil)
+odds.ratio(RLCeradBinarioSugarTercil, level=0.95)
 saveRDS(RLCeradBinarioSugarTercil, file = "~/Path/RLCeradBinarioSugarTercil.rds")
 
 #Model 1
-RLCeradBinarioSugarTercilD <- svyglm(CeradBinario ~ as.factor(AcucarTercil)+Gender+RIDAGEYR+Raca+Marital+Education+DR1TKCAL, family = quasibinomial, 
+RLCeradBinarioSugarTercilD <- svyglm(CeradBinario ~ as.factor(AcucarTercil)+Gender+Idade+Raca+Marital+Education+DR1TKCAL, family = quasibinomial, 
 design = survey19)
 summary(RLCeradBinarioSugarTercilD)
+odds.ratio(RLCeradBinarioSugarTercilD, level=0.95)
 saveRDS(RLCeradBinarioSugarTercilD , file = "~/Path/RLCeradBinarioSugarTercilD.rds")
 
 #Model 2
-RLCeradBinarioSugarTercilDC <- svyglm(CeradBinario ~ as.factor(AcucarTercil)+Gender+RIDAGEYR+Raca+Marital+Education+DR1TKCAL+BMXBMI+DiabetesYN+HipertensaoYN+
+RLCeradBinarioSugarTercilDC <- svyglm(CeradBinario ~ as.factor(AcucarTercil)+Gender+Idade+Raca+Marital+Education+DR1TKCAL+BMXBMI+DiabetesYN+HipertensaoYN+
 AlcoholYN+SmokeYN+Exercise, family = quasibinomial, design = survey19)
 summary(RLCeradBinarioSugarTercilDC)
+odds.ratio(RLCeradBinarioSugarTercilDC, level=0.95)
 saveRDS(RLCeradBinarioSugarTercilDC , file = "~/Path/RLCeradBinarioSugarTercilDC.rds")
 
 
@@ -25,18 +28,21 @@ saveRDS(RLCeradBinarioSugarTercilDC , file = "~/Path/RLCeradBinarioSugarTercilDC
 #Unadjusted
 RLAnimalBinarioSugarTercil <- svyglm(AnimalBinario ~ as.factor(AcucarTercil), family = quasibinomial, design = survey19)
 summary(RLAnimalBinarioSugarTercil)
+odds.ratio(RLAnimalBinarioSugarTercil, level=0.95)
 saveRDS(RLAnimalBinarioSugarTercil, file = "~/Path/RLAnimalBinarioSugarTercil.rds")
 
 #Model 1
-RLAnimalBinarioSugarTercilD <- svyglm(AnimalBinario ~ as.factor(AcucarTercil)+Gender+RIDAGEYR+Raca+Marital+Education+DR1TKCAL, family = quasibinomial, 
+RLAnimalBinarioSugarTercilD <- svyglm(AnimalBinario ~ as.factor(AcucarTercil)+Gender+Idade+Raca+Marital+Education+DR1TKCAL, family = quasibinomial, 
 design = survey19)
 summary(RLAnimalBinarioSugarTercilD)
+odds.ratio(RLAnimalBinarioSugarTercilD, level=0.95)
 saveRDS(RLAnimalBinarioSugarTercilD , file = "~/Path/RLAnimalBinarioSugarTercilD.rds")
 
 #Model 2
-RLAnimalBinarioSugarTercilDC <- svyglm(AnimalBinario ~ as.factor(AcucarTercil)+Gender+RIDAGEYR+Raca+Marital+Education+DR1TKCAL+BMXBMI+DiabetesYN+HipertensaoYN+
+RLAnimalBinarioSugarTercilDC <- svyglm(AnimalBinario ~ as.factor(AcucarTercil)+Gender+Idade+Raca+Marital+Education+DR1TKCAL+BMXBMI+DiabetesYN+HipertensaoYN+
 AlcoholYN+SmokeYN+Exercise, family = quasibinomial, design = survey19)
 summary(RLAnimalBinarioSugarTercilDC)
+odds.ratio(RLAnimalBinarioSugarTercilDC, level=0.95)
 saveRDS(RLAnimalBinarioSugarTercilDC , file = "~/Path/RLAnimalBinarioSugarTercilDC.rds")
 
 
@@ -44,18 +50,21 @@ saveRDS(RLAnimalBinarioSugarTercilDC , file = "~/Path/RLAnimalBinarioSugarTercil
 #Unadjusted
 RLDigitSymbolBinarioSugarTercil <- svyglm(DigitSymbolBinario ~ as.factor(AcucarTercil), family = quasibinomial, design = survey19)
 summary(RLDigitSymbolBinarioSugarTercil)
+odds.ratio(RLDigitSymbolBinarioSugarTercil, level=0.95)
 saveRDS(RLDigitSymbolBinarioSugarTercil, file = "~/Path/RLDigitSymbolBinarioSugarTercil.rds")
 
 #Model 1
-RLDigitSymbolBinarioSugarTercilD <- svyglm(DigitSymbolBinario ~ as.factor(AcucarTercil)+Gender+RIDAGEYR+Raca+Marital+Education+DR1TKCAL, family = quasibinomial,
+RLDigitSymbolBinarioSugarTercilD <- svyglm(DigitSymbolBinario ~ as.factor(AcucarTercil)+Gender+Idade+Raca+Marital+Education+DR1TKCAL, family = quasibinomial,
 design = survey19)
 summary(RLDigitSymbolBinarioSugarTercilD)
+odds.ratio(RLDigitSymbolBinarioSugarTercilD, level=0.95)
 saveRDS(RLDigitSymbolBinarioSugarTercilD , file = "~/Path/RLDigitSymbolBinarioSugarTercilD.rds")
 
 #Model 2
-RLDigitSymbolBinarioSugarTercilDC <- svyglm(DigitSymbolBinario ~ as.factor(AcucarTercil)+Gender+RIDAGEYR+Raca+Marital+Education+DR1TKCAL+BMXBMI+DiabetesYN+
+RLDigitSymbolBinarioSugarTercilDC <- svyglm(DigitSymbolBinario ~ as.factor(AcucarTercil)+Gender+Idade+Raca+Marital+Education+DR1TKCAL+BMXBMI+DiabetesYN+
 HipertensaoYN+AlcoholYN+SmokeYN+Exercise, family = quasibinomial, design = survey19)
 summary(RLDigitSymbolBinarioSugarTercilDC)
+odds.ratio(RLDigitSymbolBinarioSugarTercilDC, level=0.95)
 saveRDS(RLDigitSymbolBinarioSugarTercilDC , file = "~/Path/RLDigitSymbolBinarioSugarTercilDC.rds")
 
 
@@ -63,16 +72,19 @@ saveRDS(RLDigitSymbolBinarioSugarTercilDC , file = "~/Path/RLDigitSymbolBinarioS
 #Unadjusted
 RLCognicaoBinarioSugarTercil <- svyglm(CognicaoBinario ~ as.factor(AcucarTercil), family = quasibinomial, design = survey19)
 summary(RLCognicaoBinarioSugarTercil)
+odds.ratio(RLCognicaoBinarioSugarTercil, level=0.95)
 saveRDS(RLCognicaoBinarioSugarTercil, file = "~/Path/RLCognicaoBinarioSugarTercil.rds")
 
 #Model 1
-RLCognicaoBinarioSugarTercilD <- svyglm(CognicaoBinario ~ as.factor(AcucarTercil)+Gender+RIDAGEYR+Raca+Marital+Education+DR1TKCAL, family = quasibinomial, 
+RLCognicaoBinarioSugarTercilD <- svyglm(CognicaoBinario ~ as.factor(AcucarTercil)+Gender+Idade+Raca+Marital+Education+DR1TKCAL, family = quasibinomial, 
 design = survey19)
 summary(RLCognicaoBinarioSugarTercilD)
+odds.ratio(RLCognicaoBinarioSugarTercilD, level=0.95)
 saveRDS(RLCognicaoBinarioSugarTercilD , file = "~/Path/RLCognicaoBinarioSugarTercilD.rds")
 
 #Model 2
-RLCognicaoBinarioSugarTercilDC <- svyglm(CognicaoBinario ~ as.factor(AcucarTercil)+Gender+RIDAGEYR+Raca+Marital+Education+DR1TKCAL+BMXBMI+DiabetesYN+
+RLCognicaoBinarioSugarTercilDC <- svyglm(CognicaoBinario ~ as.factor(AcucarTercil)+Gender+Idade+Raca+Marital+Education+DR1TKCAL+BMXBMI+DiabetesYN+
 HipertensaoYN+AlcoholYN+SmokeYN+Exercise, family = quasibinomial, design = survey19)
 summary(RLCognicaoBinarioSugarTercilDC)
+odds.ratio(RLCognicaoBinarioSugarTercilDC, level=0.95)
 saveRDS(RLCognicaoBinarioSugarTercilDC , file = "~/Path/RLCognicaoBinarioSugarTercilDC.rds")
